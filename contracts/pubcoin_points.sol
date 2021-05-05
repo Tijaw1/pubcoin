980783e74dfbd567d30b0b44ae2f96f3df6f2518
Blake Cizek <b.cizek@f5.com>
2021-05-04 21:05:12 -0700

    push

--- contracts/pubcoin_points.sol
+++ contracts/pubcoin_points.sol
@@ -8,5 +8,25 @@ contract pubcoin_points {
     constructor() public {
         owner = msg.sender;
     }
+    
+    struct customer {
+        address customer_id;
+        string first_name;
+        string last_name;
+        string email;
+        bool isReg;
+        mapping (address => bool) bus;
+    }
+    
+    struct business {
+        address business_id;
+        string business_name;
+        string email;
+        bool isReg;
+        pubcoin_points pp;
+        mapping(address => bool) cust;
+        mapping(address ==> bool) partnership;
+        mapping(address ==> uint256) xrate
+    }
 }
 
