ba53319b4649ce4f7c2ba714af3ecea4cce85bd4
Blake Cizek <b.cizek@f5.com>
2021-05-04 20:44:41 -0700

    push

--- contracts/pubcoin.sol
+++ contracts/pubcoin.sol
@@ -4,15 +4,25 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract PubCoin is ERC20, ERC20Detailed, ERC20Mintable {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint initial_supply
-    )
-        ERC20Detailed(name, symbol, 18)
-        public
-    {
-        // constructor can stay empty
-    }
+contract pubcoin is ERC20, ERC20Mintable {
+
+	///PubCoin programs
+	address private owner;
+	string private name;
+	string private symbol;
+	uint8 private decimal;
+
+	//the creator of the contarct is the owner
+	constructor(
+	    address _owner,
+	    string memory _name,
+	    string memory _symbol,
+	    uint8 _decimal
+	    )public
+	{
+		owner = _owner;
+		name = _name;
+		symbol = _symbol;
+		decimal = _decimal;
+	}
 }
